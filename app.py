67987e069b1ccba98d8ca896762cb35c72dd6002
Hemang Monga <52912506+hemangmonga@users.noreply.github.com>
2021-01-28 01:16:54 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,38 @@
+from flask import Flask, render_template, url_for, redirect, request, send_file
+import pickle
+import numpy as np
+import pandas as pd
+
+app = Flask(__name__)
+model = pickle.load(open('model.pickle', 'rb'))
+scaler = pickle.load(open('scaler.pickle', 'rb'))
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
+@app.route('/predict', methods=['GET', 'POST'])
+def predict():
+    input_features = [float(x) for x in request.form.values()]
+    features_value = [np.array(input_features)]
+    features_name = ['radius_mean', 'texture_mean', 'perimeter_mean',
+                     'area_mean', 'smoothness_mean', 'compactness_mean', 'concavity_mean',
+                     'concave points_mean', 'symmetry_mean', 'fractal_dimension_mean',
+                     'radius_se', 'texture_se', 'perimeter_se', 'area_se', 'smoothness_se',
+                     'compactness_se', 'concavity_se', 'concave points_se', 'symmetry_se',
+                     'fractal_dimension_se', 'radius_worst', 'texture_worst',
+                     'perimeter_worst', 'area_worst', 'smoothness_worst',
+                     'compactness_worst', 'concavity_worst', 'concave points_worst',
+                     'symmetry_worst', 'fractal_dimension_worst']
+
+    df = pd.DataFrame(features_value, columns=features_name)
+    df = scaler.transform(df)
+    output = model.predict(df)
+
+    return render_template('after.html', data=output)
+
+
+if __name__ == "__main__":
+    app.run()
